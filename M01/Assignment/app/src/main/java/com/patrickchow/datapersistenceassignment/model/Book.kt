98d28a50cbcac1c9a1b187103f0cd1913b483b38
Patrick Chow <PatrickChow0803@gmail.com>
2019-09-16 15:16:48 -0400

    Created the class Book with data members

---
+++ M01/Assignment/app/src/main/java/com/patrickchow/datapersistenceassignment/model/Book.kt
@@ -0,0 +1,10 @@
+package com.patrickchow.datapersistenceassignment.model
+
+class Book(
+    var title: String,
+    var reasonToRead: String,
+    var hasBeenRead: Boolean,
+    var id: String
+){
+    
+}
